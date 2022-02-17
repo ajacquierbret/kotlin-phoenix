bea682467b0d671d04eb5dffa11cd5b6c65f76a7
ajacquierbret <a.jacquier.bret@gmail.com>
2022-02-17 12:14:38 +0100

    [Apollo Adapter] Add reconnectWhith param & handle backgroundDispatcher

--- kotlinphoenix-adapters/src/commonMain/kotlin/io/github/ajacquierbret/kotlinphoenix/adapters/apollo/PhoenixNetworkTransport.kt
+++ kotlinphoenix-adapters/src/commonMain/kotlin/io/github/ajacquierbret/kotlinphoenix/adapters/apollo/PhoenixNetworkTransport.kt
@@ -34,8 +34,8 @@ internal sealed interface PhxEvent : PhxMessage {
 
 internal class OperationResponse(override val id: String?, val payload: Map<String, Any?>) :
     PhxEvent
-internal class OperationError(override val id: String?, val payload: Map<String, Any?>?) : PhxEvent
 internal class OperationComplete(override val id: String?) : PhxEvent
+internal class OperationError(override val id: String?, val payload: Map<String, Any?>?) : PhxEvent
 internal class GeneralError(val payload: Map<String, Any?>?) : PhxEvent {
     override val id: String? = null
 }
@@ -55,13 +55,17 @@ internal class NetworkError(val cause: Throwable) : PhxEvent {
  * @param reconnectWhen a function taking the error as a parameter and returning 'true' to reconnect
  *
  */
+@Suppress("unused")
 class PhoenixNetworkTransport private constructor(
     private val serverUrl: String,
     private val topic: String,
-    private val params: Payload? = null,
+    private var params: Payload? = null,
+    private var logger: ((log: Any) -> Unit)?,
     private val channelJoinTimeoutMs: Long = 10_000,
     private val idleTimeoutMillis: Long = 60_000,
     private val reconnectWhen: ((Throwable) -> Boolean)?,
+    private val reconnectWith: (() -> Payload?)?,
+    private val queueMessages: Boolean = true
 ) : NetworkTransport {
     /**
      * Use unlimited buffers so that we never have to suspend when writing a command or an event,
@@ -77,11 +81,23 @@ class PhoenixNetworkTransport private constructor(
 
     private val activeSubscriptions = mutableMapOf<String, String>()
 
-    private val coroutineScope = CoroutineScope(Dispatchers.Default)
+    private val backgroundDispatcher = BackgroundDispatcher()
+    private val coroutineScope = CoroutineScope(backgroundDispatcher.coroutineDispatcher)
+
+    /**
+     * No need to lock these variables as they are all accessed from the same thread
+     */
+    private var idleJob: Job? = null
+    private var connectionJob: Job? = null
+    private val activeMessages = mutableMapOf<Uuid, StartOperation<*>>()
+
+    private var socket: PhxSocket? = null
+    private var channel: PhxChannel? = null
 
     init {
         coroutineScope.launch {
             supervise(this)
+            backgroundDispatcher.dispose()
         }
     }
 
@@ -203,52 +219,92 @@ class PhoenixNetworkTransport private constructor(
         }
     }
 
+    private suspend fun connectionInit(scope: CoroutineScope) {
+        val phxSocket = PhxSocket(url = serverUrl, params = params, scope = scope)
+        socket = phxSocket
+
+        phxSocket.logger = {
+            logger?.invoke(it)
+        }
+
+        val connection = try {
+            phxSocket.connect()
+        } catch (error: SocketEvent.FailureEvent) {
+            // Error opening the socket
+            mutableEvents.emit(NetworkError(error.throwable))
+            throw error
+        }
+
+        connectionJob = scope.launch(start = CoroutineStart.UNDISPATCHED) {
+            connection?.collect {
+                when (it) {
+                    is SocketEvent.MessageEvent ->
+                        handleServerMessage(
+                            absintheMessageToApolloMessage(
+                                message = it.text,
+                                type = "ok"
+                            )
+                        )
+
+                    is SocketEvent.FailureEvent -> listener.networkError(it.throwable)
+                    else -> Unit
+                }
+            }
+        }
+
+        channel = phxSocket.channel(topic, connection!!)
+
+        try {
+            withTimeout(channelJoinTimeoutMs) {
+                launch {
+                    channel?.join()?.collect {
+                        when (it.status) {
+                            "ok" -> cancel()
+                            "error" -> throw SocketEvent.FailureEvent(Throwable("Connection error while joining phoenix channel : $it"), null)
+                            else -> println("Unhandled message status while joining phoenix channel : ${it.status}")
+                        }
+                    }
+                }
+            }
+        } catch (error: SocketEvent.FailureEvent) {
+            // Error initializing the connection
+            socket = null
+            channel = null
+            mutableEvents.emit(NetworkError(error.throwable))
+            throw error
+        }
+    }
+
+    /**
+     * This happens:
+     * - when this coroutine receives a [Dispose] message
+     * - when the idleJob completes
+     * - when there is an error reading the socket and this coroutine receives a [NetworkError] message
+     */
+    private suspend fun closeTransport() {
+        connectionJob?.cancel()
+        connectionJob = null
+        channel?.leave()
+        socket?.disconnect()
+        channel = null
+        socket = null
+        idleJob?.cancel()
+        idleJob = null
+    }
+
     /**
      * Long-running method that creates/handles the socket lifecyle
      */
     private suspend fun supervise(scope: CoroutineScope) {
-        /**
-         * No need to lock these variables as they are all accessed from the same thread
-         */
-        var idleJob: Job? = null
-        var connectionJob: Job? = null
-        val activeMessages = mutableMapOf<Uuid, StartOperation<*>>()
-
-        var socket: PhxSocket? = null
-        var channel: PhxChannel? = null
-
-        /**
-         * This happens:
-         * - when this coroutine receives a [Dispose] message
-         * - when the idleJob completes
-         * - when there is an error reading the socket and this coroutine receives a [NetworkError] message
-         */
-        suspend fun closeTransport() {
-            connectionJob?.cancel()
-            connectionJob = null
-            channel?.leave()
-            socket?.disconnect()
-            channel = null
-            socket = null
-            idleJob?.cancel()
-            idleJob = null
-        }
-
         while (true) {
-            val message = messages.receive()
 
-            when (message) {
+            when (val message = messages.receive()) {
                 is PhxEvent -> {
                     if (message is NetworkError) {
                         closeTransport()
 
                         if (reconnectWhen?.invoke(message.cause) == true) {
-                            activeMessages.values.forEach {
-                                // Re-queue all start messages
-                                // Will reconnect the socket and
-                                // try to rejoin the channel
-                                messages.trySend(it)
-                            }
+                            reconnect(scope, queueMessages)
                         } else {
                             // forward the NetworkError downstream. Active flows will throw
                             mutableEvents.tryEmit(message)
@@ -264,52 +320,9 @@ class PhoenixNetworkTransport private constructor(
                             continue
                         }
 
-                        socket = PhxSocket(url = serverUrl, params = params, scope = scope)
-
-                        val connection = try {
-                            socket!!.connect()
-                        } catch (e: SocketEvent.FailureEvent) {
-                            // Error opening the socket
-                            mutableEvents.emit(NetworkError(e))
-                            continue
-                        }
-
-                        connectionJob = scope.launch(start = CoroutineStart.UNDISPATCHED) {
-                            connection!!.collect {
-                                when (it) {
-                                    is SocketEvent.MessageEvent ->
-                                        handleServerMessage(
-                                            absintheMessageToApolloMessage(
-                                                message = it.text,
-                                                type = "ok"
-                                            )
-                                        )
-
-                                    is SocketEvent.FailureEvent -> listener.networkError(it.throwable)
-                                    else -> Unit
-                                }
-                            }
-                        }
-
-                        channel = socket!!.channel(topic, connection!!)
-
                         try {
-                            withTimeout(channelJoinTimeoutMs) {
-                                launch {
-                                    channel!!.join().collect {
-                                        when (it.status) {
-                                            "ok" -> cancel()
-                                            "error" -> throw SocketEvent.FailureEvent(Throwable("Connection error while joining phoenix channel : $it"), null)
-                                            else -> println("Unhandled message status while joining phoenix channel : ${it.status}")
-                                        }
-                                    }
-                                }
-                            }
+                            connectionInit(scope)
                         } catch (e: SocketEvent.FailureEvent) {
-                            // Error initializing the connection
-                            socket = null
-                            channel = null
-                            mutableEvents.emit(NetworkError(e))
                             continue
                         }
                     }
@@ -341,6 +354,29 @@ class PhoenixNetworkTransport private constructor(
                     }
                 }
             }
+        }
+    }
+
+    @Suppress()
+    suspend fun reconnect(
+        scope: CoroutineScope,
+        queueMessages: Boolean = true,
+    ) {
+        closeTransport()
+
+        reconnectWith?.let {
+            params = it.invoke()
+        }
+
+        if (queueMessages) {
+            activeMessages.values.forEach {
+                // Re-queue all start messages
+                // Will reconnect the socket and
+                // try to rejoin the channel
+                messages.trySend(it)
+            }
+        } else {
+            connectionInit(scope)
         }
     }
 
@@ -391,19 +427,24 @@ class PhoenixNetworkTransport private constructor(
         messages.trySend(Dispose)
     }
 
+    @Suppress("unused")
     class Builder {
         private var serverUrl: String? = null
         private var topic: String? = null
         private var params: Payload? = null
+        private var logger: ((log: Any) -> Unit)? = null
         private var channelJoinTimeoutMs: Long? = null
         private var idleTimeoutMillis: Long? = null
         private var reconnectWhen: ((Throwable) -> Boolean)? = null
+        private var reconnectWith: (() -> Payload?)? = null
+        private var queueMessages: Boolean? = null
 
         /**
          * Set the server url
          *
          * @param serverUrl the url to use to establish the socket connection.
          */
+        @Suppress("unused")
         fun serverUrl(serverUrl: String) = apply {
             this.serverUrl = serverUrl
         }
@@ -413,6 +454,7 @@ class PhoenixNetworkTransport private constructor(
          *
          * @param topic the topic to which the channel will subscribe
          */
+        @Suppress("unused")
         fun topic(topic: String) = apply {
             this.topic = topic
         }
@@ -422,8 +464,14 @@ class PhoenixNetworkTransport private constructor(
          *
          * @param params the parameters to send to the server (e.g. authentication headers)
          */
+        @Suppress("unused")
         fun params(params: Payload) = apply {
             this.params = params
+        }
+
+        @Suppress("unused")
+        fun logger(logger: ((log: Any) -> Unit)?) = apply {
+            this.logger = logger
         }
 
         /**
@@ -431,6 +479,7 @@ class PhoenixNetworkTransport private constructor(
          *
          * @param channelJoinTimeoutMs the time during which the channel will try to join before timing out
          */
+        @Suppress("unused")
         fun channelJoinTimeoutMs(channelJoinTimeoutMs: Long) = apply {
             this.channelJoinTimeoutMs = channelJoinTimeoutMs
         }
@@ -440,30 +489,47 @@ class PhoenixNetworkTransport private constructor(
          *
          * @param idleTimeoutMillis the idle time duration beyond which the socket will disconnect
          */
+        @Suppress("unused")
         fun idleTimeoutMillis(idleTimeoutMillis: Long) = apply {
             this.idleTimeoutMillis = idleTimeoutMillis
         }
 
         /**
-         * Configure the [PhoenixNetworkTransport] to reconnect the socket automatically when a network error
-         * happens
+         * Configure the [PhoenixNetworkTransport] to reconnect the socket automatically when a network error happens.
          *
+         * @param queueMessages a boolean indicating wether or not all already registered subscriptions should try to subscribe again
          * @param reconnectWhen a function taking the error as a parameter and returning 'true' to reconnect
          * automatically or 'false' to forward the error to all listening [Flow]
          *
          */
-        fun reconnectWhen(reconnectWhen: ((Throwable) -> Boolean)?) = apply {
+        @Suppress("unused")
+        fun reconnectWhen(queueMessages: Boolean = true, reconnectWhen: ((Throwable) -> Boolean)?) = apply {
             this.reconnectWhen = reconnectWhen
+            this.queueMessages = queueMessages
+        }
+
+        /**
+         * Configure the [reconnectWhen] lambda to apply the returned params to the next socket reconnection.
+         * Can be useful in order to reconnect to the server with authorization headers applied
+         *
+         * @param reconnectWith a function returning a [Payload] that will be passed to the Phoenix Socket as a payload parameter
+         */
+        @Suppress("unused")
+        fun reconnectWith(reconnectWith: (() -> Payload?)?) = apply {
+            this.reconnectWith = reconnectWith
         }
 
         fun build(): PhoenixNetworkTransport {
             return PhoenixNetworkTransport(
-                serverUrl ?: error("No serverUrl specified"),
-                topic ?: error("No topic specified"),
-                params,
-                channelJoinTimeoutMs ?: 10_000,
-                idleTimeoutMillis ?: 60_000,
-                reconnectWhen
+                serverUrl = serverUrl ?: error("No serverUrl specified"),
+                topic = topic ?: error("No topic specified"),
+                params = params,
+                logger = logger,
+                channelJoinTimeoutMs = channelJoinTimeoutMs ?: 10_000,
+                idleTimeoutMillis = idleTimeoutMillis ?: 60_000,
+                reconnectWhen = reconnectWhen,
+                reconnectWith = reconnectWith,
+                queueMessages = queueMessages ?: true
             )
         }
     }
